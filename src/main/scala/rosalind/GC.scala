b2526b9ed46229a0829de5376487a7ec92bd9595
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-02-06 01:41:21 +0200

    GC

--- src/main/scala/rosalind/GC.scala
+++ src/main/scala/rosalind/GC.scala
@@ -6,5 +6,7 @@ import subscript.Predef._
 import rosalind.util._
 
 object GC extends SubScriptArgsApp with FileIO {script..
-  live(args: Array[String]) = fasta(args.head) ~~(smth)~~> println: smth
+  live(args: Array[String]) = fasta(args.head) ~~(data: List[(String, String)])~~^
+    data.map {case (name, str) => name -> (str.count(c => c == 'C' || c == 'G') / str.size.toDouble * 100)}.maxBy(_._2)
+    ~~((name: String, percentage: Double))~~> [println: name println: percentage]
 }
